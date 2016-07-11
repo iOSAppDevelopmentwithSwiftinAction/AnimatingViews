459137c63c4c58bbed0fabd8100ff15693377004
Craig Grummitt <craiggrummitt@gmail.com>
2016-07-11 18:18:15 -0500

    Initial Commit

---
+++ AnimateButtonTests/AnimateButtonTests.swift
@@ -0,0 +1,36 @@
+//
+//  AnimateButtonTests.swift
+//  AnimateButtonTests
+//
+//  Created by Craig Grummitt on 11/07/2016.
+//  Copyright © 2016 interactivecoconut. All rights reserved.
+//
+
+import XCTest
+@testable import AnimateButton
+
+class AnimateButtonTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
