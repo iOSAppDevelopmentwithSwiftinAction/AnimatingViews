d010a44fe300437038dbeb0f3c107e50e1836fae
Craig Grummitt <craiggrummitt@gmail.com>
2016-07-12 14:38:27 -0500

    First setup

--- AnimateButton/ViewController.swift
+++ AnimateButton/ViewController.swift
@@ -10,11 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var yellowView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
+    @IBAction func touchGoButton(_ sender: AnyObject) {
 
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -22,4 +25,3 @@ class ViewController: UIViewController {
 
 
 }
-
