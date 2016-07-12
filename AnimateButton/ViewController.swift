db2e53cb000dbe0459dd9bad7373e10d19dc28f3
Craig Grummitt <craiggrummitt@gmail.com>
2016-07-12 18:23:49 -0500

    Update initial setup

--- AnimateButton/ViewController.swift
+++ AnimateButton/ViewController.swift
@@ -9,14 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var yellowView: UIView!
+    
+    @IBOutlet weak var bar1: UIView!
+    @IBOutlet weak var bar2: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.bar1.isHidden = true
+        self.bar2.isHidden = true
     }
     @IBAction func touchGoButton(_ sender: AnyObject) {
-
+        self.bar1.isHidden = false
+        self.bar2.isHidden = false
+        
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
