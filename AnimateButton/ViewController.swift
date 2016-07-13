eafa7ae0a821a5b5bbe01d2aba81d7e997c7cd64
Craig Grummitt <craiggrummitt@gmail.com>
2016-07-13 08:43:19 -0500

    Added animations, constraints

--- AnimateButton/ViewController.swift
+++ AnimateButton/ViewController.swift
@@ -19,6 +19,28 @@ class ViewController: UIViewController {
     @IBAction func touchViewButton(_ sender: AnyObject) {
         self.bar1.frame.size.width = 0
         self.bar2.frame.size.width = 0
+        UIView.animate(withDuration: 1,
+            animations: {
+            self.bar1.backgroundColor = UIColor.red()
+            self.bar1.frame.size.width = 150
+            }, completion: { finished in
+                UIView.animate(withDuration: 1, animations: {
+                    self.bar2.backgroundColor = UIColor.orange()
+                    self.bar2.frame.size.width = 150
+                    }, completion: { finished in
+                        let label1 = UILabel(frame: self.bar1.frame)
+                        label1.textColor = UIColor.white()
+                        label1.text = "Chinese"
+                        self.view.addSubview(label1)
+                        
+                        let label2 = UILabel(frame: self.bar2.frame)
+                        label2.textColor = UIColor.white()
+                        label2.text = "Thai"
+                        self.view.addSubview(label2)
+                    }
+                )
+            }
+        )
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
