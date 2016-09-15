6c2f6f418d5c55f3fd6f64637674f6f76226f3e4
Craig Grummitt <craiggrummitt@gmail.com>
2016-09-15 16:33:22 -0500

    Updated for Swift 3

--- AnimateButton/ViewController.swift
+++ AnimateButton/ViewController.swift
@@ -21,20 +21,20 @@ class ViewController: UIViewController {
         self.bar2.frame.size.width = 0
         UIView.animate(withDuration: 1,
             animations: {
-            self.bar1.backgroundColor = UIColor.red()
+            self.bar1.backgroundColor = UIColor.red
             self.bar1.frame.size.width = 150
             }, completion: { finished in
                 UIView.animate(withDuration: 1, animations: {
-                    self.bar2.backgroundColor = UIColor.orange()
+                    self.bar2.backgroundColor = UIColor.orange
                     self.bar2.frame.size.width = 150
                     }, completion: { finished in
                         let label1 = UILabel(frame: self.bar1.frame)
-                        label1.textColor = UIColor.white()
+                        label1.textColor = UIColor.white
                         label1.text = "Chinese"
                         self.view.addSubview(label1)
                         
                         let label2 = UILabel(frame: self.bar2.frame)
-                        label2.textColor = UIColor.white()
+                        label2.textColor = UIColor.white
                         label2.text = "Thai"
                         self.view.addSubview(label2)
                     }
